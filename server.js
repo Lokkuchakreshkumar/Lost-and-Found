b94fb5cb9358d4a29417a99c5c8dce5349e94e11
Lokkuchakreshkumar <lokkuchakreshkumar@gmail.com>
2024-10-15 21:28:23 +0530

    Fix routing and configuration for Vercel

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware to serve static files from the public directory
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-// Routes for serving HTML files
+// Define routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
